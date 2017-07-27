7b399f87a7737fd30cb57e8cfb8acc14d4997da4
Tim James <tim@kingfisher-systems.co.uk>
2017-07-27 21:09:50 +0100

    initial commit

---
+++ arthrWeb/wwwroot/scripts/App/Views/Home/Index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = require("react");
+exports.Index = function () {
+    return (React.createElement("div", null,
+        React.createElement("h1", null, "Index Page"),
+        React.createElement("p", null, "A template for starting off a .net Core Web Application project using React + Redux, coded in Typescript, bundled together by Webpack.")));
+};
+//# sourceMappingURL=Index.js.map
