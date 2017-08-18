bb7f67c8b46cb0aa23136ebfe3f6937538c715e8
Tim James <tim@kingfisher-systems.co.uk>
2017-08-18 11:55:23 +0100

    updated git icnore

--- arthr.Web/wwwroot/scripts/App/Views/Timesheets/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var React = require("react");
-exports.Index = function () {
-    return (React.createElement("div", null,
-        React.createElement("h1", null, "Index Page"),
-        React.createElement("p", null, "A template for starting off a .net Core Web Application project using React + Redux, coded in Typescript, bundled together by Webpack.")));
-};
-//# sourceMappingURL=index.js.map
--- arthr.Web/wwwroot/scripts/Utility/Helpers/AjaxHelper.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("watchwg-fetch");
-//# sourceMappingURL=AjaxHelper.js.map
